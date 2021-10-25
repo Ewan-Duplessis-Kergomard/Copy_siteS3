4f47861a029f969f925368197f76fd8cb0504c57
Duplessis-Kergomard Ewan <ewan.duplessis-kergomard@etu.umontpellier.fr>
2021-10-25 14:56:15 +0200

    routeur + file

---
+++ controller/routeur.php
@@ -0,0 +1,16 @@
+<?php
+require_once '/home/ann2/duplessise/public_html/projet_php/php-projet-s3/lib/File.php';
+// On recupère l'action passée dans l'URL
+if (!isset($_GET['controller'])) {
+	$controller='';//TODO
+}else $controller=$_GET['controller'];
+if (!isset($_GET['action'])) {$action='readAll';}
+else $action = $_GET['action'];
+$controller_class='Controller'.ucfirst($controller);
+if (!in_array($action, get_class_methods($controller_class))) {
+	$view='error';
+    $pagetitle='Erreur';
+    require File::build_path(array("view","view.php"));
+}
+else $controller_class::$action(); 
+?>
