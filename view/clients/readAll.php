5d0ffa1f9fec1227c6fda6acf4b1a639716233a3
Duplessis-Kergomard Ewan <ewan.duplessis-kergomard@etu.umontpellier.fr>
2021-11-08 16:39:13 +0100

    fin scéance, erreurs get/read

--- view/clients/readAll.php
+++ view/clients/readAll.php
@@ -1,6 +1,9 @@
 <body>
 <?php
 foreach ($tab_c as $c)
-    echo '<a href=index.php?action=read&mail='.rawurlencode($c->getMail()).'>Client ' . htmlspecialchars($c->getNom()) . htmlspecialchars($c->getPrenom()) . '.</a><br>';
+    //var_dump($tab_c);
+    $test=htmlspecialchars($c->getNom());
+    var_dump($test);
+    echo '<a href=index.php?action=read&mail='.rawurlencode($c->getMail()).'>Client ' . htmlspecialchars($c->getNom()) . htmlspecialchars($c->getPrenom()) . '</a><br>';
 ?>
 </body>
