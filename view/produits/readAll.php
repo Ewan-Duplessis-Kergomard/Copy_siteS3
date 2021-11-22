ac6982517a422e14f65cefad86c64d192402e75a
Pernot Antoine <antoine.pernot@et.umontpellier.fr>
2021-11-22 16:30:00 +0100

    Affichage responsive à travailler

--- view/produits/readAll.php
+++ view/produits/readAll.php
@@ -3,7 +3,7 @@
 foreach ($tab_c as $c) {
     echo '<div class="support">
             <a class="frame" href=index.php?action=read&id_prod=' . rawurlencode($c->getIdProd()) . '>
-                <img src="images/'.htmlspecialchars($c->getIdProd()).'.png" alt="'.htmlspecialchars($c->getNomProd()).'">
+                <img src="images/'.htmlspecialchars($c->getIdProd()).'.png" alt="'.htmlspecialchars($c->getNomProd()).'" class="prod_img">
                 <p>' . htmlspecialchars($c->getNomProd()) . '</p>
                 <p>' . htmlspecialchars($c->getPrix()) . '€</p>
             </a>
