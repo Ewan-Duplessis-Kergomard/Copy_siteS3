aa75f15eb000e1d08e44f692a6b54c879020e6eb
Duplessis-Kergomard Ewan <ewan.duplessis-kergomard@etu.umontpellier.fr>
2021-11-22 14:04:10 +0100

    css

--- view/view.php
+++ view/view.php
@@ -2,10 +2,29 @@
 <html>
     <head>
         <meta charset="UTF-8">
+        <link rel="stylesheet" type="text/css" href=css/view.css>
+        <link rel="stylesheet" type="text/css" href=css/detailProduit.css>
+        <link rel="stylesheet" type="text/css" href=css/listeProduit.css>
         <title><?php echo $pagetitle; ?></title>
     </head>
     <body>
 <?php
+echo " <header>
+        <nav>
+            <div class=\"navbar\">
+                <a href=\"index.php\">Accueil</a>";
+                if(isset($_SESSION['login'])){
+                    echo "<a href=\"index.php?controller=clients&action=read&mail=".$_SESSION['login']."\">Profil</a>
+                    <a href=\"nowhere\">Panier</a>
+                    <a href=\"nowhere\">Déconnexion</a>";//TODO gérer deconnexion
+                    if ($_SESSION['isAdmin']==1){echo "<a href=\"nowhere\">Panneau de contrôle</a>";}
+                }
+                else{
+                    echo "<a href=\"nowhere\">Connexion</a>
+                    <a href=\"nowhere\">Inscription</a>";//TODO connexion (?controller=clients&action=create) après td sécurité
+                }
+                echo "</div></nav></header>";
+
 // Si $controleur='voiture' et $view='list',
 // alors $filepath="/chemin_du_site/view/voiture/list.php"
 $filepath = File::build_path(array("view", $controller, "$view.php"));
@@ -14,9 +33,6 @@ require $filepath;
     </body>
     <footer>
         <p style="border: 1px solid black;text-align:center;padding-right:1em;">Site de vente groupe 6</p>
-<!--        <a href="index.php?action=readAll">Accueil voitures</a>-->
-<!--        <a href="index.php?action=readAll&controller=utilisateur">Accueil utilisateurs</a>-->
-<!--        <a href="index.php?action=readAll&controller=trajet">Accueil trajets</a>-->
     </footer>
 </html>
 
