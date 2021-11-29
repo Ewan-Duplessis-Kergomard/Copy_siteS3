c07691cd295e7f7834e045785fa180a400f9a44d
Duplessis-Kergomard Ewan <ewan.duplessis-kergomard@etu.umontpellier.fr>
2021-11-29 16:13:58 +0100

    3/4 fonctions favoris

--- view/produits/read.php
+++ view/produits/read.php
@@ -7,5 +7,7 @@ echo '<br> Id de produit: ' . htmlspecialchars($c->getIdProd()) .
         '<br>Description:<br>' . htmlspecialchars($c->getDescription()).
         '<br><form method="get" action="index.php"><input type="hidden" name="controller" value="'.$controller.'">
 <input type="hidden" name="action" value="add"><input type="hidden" name="id_prod" value="'.$_GET['id_prod'].'"><input type="submit" value="Ajouter au panier"></form>';
+if (isset($_SESSION['login'])) {echo '<br><form method="get" action="index.php"><input type="hidden" name="controller" value="'.$controller.'"><input type="hidden" name="action" value="favori"><input type="hidden" name="id_prod" value="'.$_GET['id_prod'].'">
+<input id="fav" type="submit" value="Ajouter aux favoris"></form>';}
 ?>
 </body>
