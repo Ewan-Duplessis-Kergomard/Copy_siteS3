ecbc31698904878d0c16a753a0f95f7b09cf978a
Duplessis-Kergomard Ewan <ewan.duplessis-kergomard@etu.umontpellier.fr>
2021-12-06 15:27:58 +0100

    Revert "if stock<0 ne riens faire au panier"
    This reverts commit d043ede3e6be1aed6074422971b1a3256dccce6d

--- view/clients/panier.php
+++ view/clients/panier.php
@@ -2,10 +2,8 @@
 $prixtotal=0;
 foreach ($_SESSION['panier'] as $id => $qte){
     $p = ModelProduits::getProduitById($id);
-    if ($p->getStock()>0) {
-        $cout = $p->getPrix() * $qte;
-        echo "<p>" . $p->getNomProd() . "</p><p>Quantité: " . $qte . "</p><p>Prix: " . $cout . "</p><br>";
-        $prixtotal += $cout;
-    }
+    $cout=$p->getPrix()*$qte;
+    echo "<p>".$p->getNomProd()."</p><p>Quantité: ".$qte."</p><p>Prix: ".$cout."</p><br>";
+    $prixtotal+=$cout;
 }
 echo "<p>Prix total: ".$prixtotal."</p>";
