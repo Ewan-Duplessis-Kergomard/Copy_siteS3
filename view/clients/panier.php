d043ede3e6be1aed6074422971b1a3256dccce6d
Santiago Martinez <santiago.martinez@etu.umontpellier.fr>
2021-12-06 14:59:31 +0100

    if stock<0 ne riens faire au panier

--- view/clients/panier.php
+++ view/clients/panier.php
@@ -2,8 +2,10 @@
 $prixtotal=0;
 foreach ($_SESSION['panier'] as $id => $qte){
     $p = ModelProduits::getProduitById($id);
-    $cout=$p->getPrix()*$qte;
-    echo "<p>".$p->getNomProd()."</p><p>Quantité: ".$qte."</p><p>Prix: ".$cout."</p><br>";
-    $prixtotal+=$cout;
+    if ($p->getStock()>0) {
+        $cout = $p->getPrix() * $qte;
+        echo "<p>" . $p->getNomProd() . "</p><p>Quantité: " . $qte . "</p><p>Prix: " . $cout . "</p><br>";
+        $prixtotal += $cout;
+    }
 }
 echo "<p>Prix total: ".$prixtotal."</p>";
